d0379e7b90b4d3c90cded6d8640ce027ca145533
guillaumepoucet <61963476+guillaumepoucet@users.noreply.github.com>
2024-01-18 14:42:26 +0100

    Add game list and player details to games page

--- src/app/games/page.jsx
+++ src/app/games/page.jsx
@@ -1,1 +1,34 @@
-// TODO afficher liste des games
+import { games } from "@component/lib/games";
+import { formatDate } from "@component/utils/formatDate";
+import Link from "next/link"
+
+function Page() {
+  return (
+    <div>
+      <h1>Games</h1>
+
+      <ul className="grid space-y-5">
+        {games.map((game) => {
+          return (
+            <li key={game.id}>
+              <p>{formatDate(new Date(game.date))}</p>
+              <p>{game.winner.pseudo}</p>
+              <div>Players:
+                <ul>
+                  {game.players.map((player) => (
+                    <li key={player.id}>{player.pseudo}</li>
+                  ))}
+                </ul>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+
+      <Link href="/games/new">Add game</Link>
+
+    </div>
+  )
+}
+
+export default Page
